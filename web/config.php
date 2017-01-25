32b39bad1dcc89d140d2a12a6d9eb388fde95882
AlekseyChepurko <aleksey.chepurko@gmail.com>
2017-01-25 14:41:44 +0300

    conposer updated

--- web/config.php
+++ web/config.php
@@ -16,8 +16,6 @@ if (!isset($_SERVER['HTTP_HOST'])) {
 
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
-    '92.53.96.95',
-    '92.100.228.42',
     '::1',
 ))) {
     header('HTTP/1.0 403 Forbidden');
